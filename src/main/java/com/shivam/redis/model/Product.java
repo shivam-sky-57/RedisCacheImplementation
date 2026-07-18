066b2a7936eac65bd70cad24c3f5eca91c6dc9e5
shivam-sky-57 <shivamkumar7965@gmail.com>
2026-07-18 16:23:27 +0100

    product model initialized

--- src/main/java/com/shivam/redis/model/Product.java
+++ src/main/java/com/shivam/redis/model/Product.java
@@ -1,5 +1,27 @@
 package com.shivam.redis.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collation = "products")
 public class Product {
-    
+    @Id
+    private String id;
+
+    private String name;
+
+    private double price;
+
+    private int quantity;
 }
