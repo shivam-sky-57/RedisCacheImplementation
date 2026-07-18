79622c933ab2fd6569d0c74e59e42503e517d93c
shivam-sky-57 <shivamkumar7965@gmail.com>
2026-07-18 16:37:42 +0100

    service and repository layer  initialized

---
+++ src/main/java/com/shivam/redis/service/ProductService.java
@@ -0,0 +1,33 @@
+package com.shivam.redis.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.shivam.redis.model.Product;
+import com.shivam.redis.repository.ProductRepository;
+
+@Service
+public class ProductService {
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public Product save(Product product) {
+        return repository.save(product);
+    }
+
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+
+    public Product getById(String id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void delete(String id) {
+        repository.deleteById(id);
+    }
+}
