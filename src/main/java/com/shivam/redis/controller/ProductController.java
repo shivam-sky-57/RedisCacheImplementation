e97d93ca89f0f3b93d0910b7dcad3c2688eb1585
shivam-sky-57 <shivamkumar7965@gmail.com>
2026-07-19 05:33:53 +0100

    product controllerinitialized

---
+++ src/main/java/com/shivam/redis/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.shivam.redis.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shivam.redis.model.Product;
+import com.shivam.redis.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return service.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable String id) {
+        return service.getById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        service.delete(id);
+    }
+}
