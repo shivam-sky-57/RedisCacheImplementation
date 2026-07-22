071afdc524aca5c058348f942f2fd1f14d22c706
shivam-sky-57 <shivamkumar7965@gmail.com>
2026-07-22 10:43:11 +0100

    using annotations

--- src/main/java/com/shivam/redis/service/ProductService.java
+++ src/main/java/com/shivam/redis/service/ProductService.java
@@ -1,9 +1,10 @@
 package com.shivam.redis.service;
 
-import java.time.Duration;
 import java.util.List;
 
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.shivam.redis.model.Product;
@@ -13,63 +14,36 @@ import com.shivam.redis.repository.ProductRepository;
 public class ProductService {
 
     private final ProductRepository repository;
-    private final RedisTemplate<String, Object> redisTemplate;
 
-    public ProductService(ProductRepository repository,
-                          RedisTemplate<String, Object> redisTemplate) {
+    public ProductService(ProductRepository repository) {
         this.repository = repository;
-        this.redisTemplate = redisTemplate;
     }
 
-    // CREATE
+    @CachePut(value = "products", key = "#result.id")
     public Product save(Product product) {
-        Product saved = repository.save(product);
 
-        // store in redis
-        redisTemplate.opsForValue()
-                .set("product:" + saved.getId(), saved, Duration.ofMinutes(10));
+        System.out.println("Saved to MongoDB");
 
-        return saved;
+        return repository.save(product);
     }
 
-    // READ ALL
     public List<Product> getAll() {
         return repository.findAll();
     }
 
-    // READ BY ID (with Redis cache)
+    @Cacheable(value = "products", key = "#id")
     public Product getById(String id) {
 
-        String key = "product:" + id;
+        System.out.println("Fetched from MongoDB");
 
-        // 1. Check Redis
-        Product cached = (Product) redisTemplate.opsForValue().get(key);
-
-        if (cached != null) {
-            System.out.println("Fetched from REDIS");
-            return cached;
-        }
-
-        // 2. If not found, fetch from MongoDB
-        Product product = repository.findById(id).orElse(null);
-
-        if (product != null) {
-            System.out.println("Fetched from MONGODB");
-
-            // 3. Store in Redis
-            redisTemplate.opsForValue()
-                    .set(key, product, Duration.ofMinutes(10));
-        }
-
-        return product;
+        return repository.findById(id).orElse(null);
     }
 
-    // DELETE
+    @CacheEvict(value = "products", key = "#id")
     public void delete(String id) {
 
         repository.deleteById(id);
 
-        // remove from redis
-        redisTemplate.delete("product:" + id);
+        System.out.println("Deleted from MongoDB");
     }
 }
